8ba71091661c7e93c76addce268e5fab6b1e4b0f
Trevor Williams <tmw7991@gmail.com>
2024-02-23 10:21:16 -0700

    feat: tailwind intellisense

--- components/modal.tsx
+++ components/modal.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function Modal({ children }: { children: ReactNode }) {
   return (
-    <div className="fixed inset-0 z-10 bg-black bg-opacity-50 flex items-center justify-center">
+    <div className=" inset-0 z-10 bg-black bg-opacity-50 flex items-center justify-center fixed">
       <div className="min-w-[24rem] bg-white p-8 rounded-lg">{children}</div>
     </div>
   );
